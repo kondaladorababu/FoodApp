a80299b2832d32e1342c6bcbc0d89f828c460a66
Dora Kondala <kondaladorababu@gmail.com>
2023-10-09 21:34:03 +0530

    Meal Form onthe Meal Item Added

--- src/Components/Meals/MealItem/Mealtem.js
+++ src/Components/Meals/MealItem/Mealtem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
@@ -12,7 +13,7 @@ const MealItem = (props) => {
             </div>
 
             <div>
-
+                <MealItemForm/>
             </div>
 
         </li>
