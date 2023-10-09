bb4eab1824b3a94691b7f72e533bde3e8e339cba
Dora Kondala <kondaladorababu@gmail.com>
2023-10-09 22:03:28 +0530

    Modal added & Portal concept Used

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,32 @@
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+    const dummyData = [{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 }];
+    const cartItems = <ul className={classes['cart-items']}>
+        {
+            dummyData.map((item) => (
+                <li>
+                    {item.name}
+                </li>
+            ))
+
+        }
+    </ul>
+    return (
+        <Modal>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>34.56</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}> Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+
+        </Modal>
+    );
+};
+
+export default Cart;
