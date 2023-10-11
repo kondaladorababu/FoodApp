154ff2ded37c43eacca7a1e4ae77c29db7586d66
Dora Kondala <kondaladorababu@gmail.com>
2023-10-11 22:48:45 +0530

    Inmplementation of UseReducer in CartProvider & Ref , forwardRef in Meal Item components

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,32 +1,25 @@
 import { useContext } from 'react';
-import CardIcon from '../Cart/CartIcon'
-import classes from './HeaderCartButton.module.css';
+
+import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-    // const handleCard = ()=>{
-    //     props.onClick(true);
-    // }
+  const cartCtx = useContext(CartContext);
 
-    const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-        return curNumber + item.amount;
-    }, 0);
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
 
-    return (
-        <button onClick={props.onClick} className={classes.button}>
-            <span className={classes.icon}>
-                <CardIcon />
-            </span>
-            <span>
-                Your Cart
-            </span>
-            <span className={classes.badge}>
-                {numberOfCartItems}
-            </span>
-        </button>
-    );
-
-}
 export default HeaderCartButton;
